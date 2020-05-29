cbe2a871775a596a2dc4b9ff89a398084946cf79
Sonu Khan <64645655+sartaj-code@users.noreply.github.com>
2020-05-29 18:57:07 +0530

    Create stack_with_arr.py

---
+++ Stack/stack_with_arr.py
@@ -0,0 +1,48 @@
+class Stack:
+
+    def __init__(self, initial_size = 10):
+        self.arr = [0 for _ in range(initial_size)]
+        self.next_index = 0
+        self.num_elements = 0
+
+    def push(self, data):
+        if self.next_index == len(self.arr):
+            print("Out of space! Increasing array capacity ...")
+            self._handle_stack_capacity_full()
+
+        self.arr[self.next_index] = data
+        self.next_index += 1
+        self.num_elements += 1
+
+    def pop(self):
+        if self.is_empty():
+            self.next_index = 0
+            return None
+        self.next_index -= 1
+        self.num_elements -= 1
+        return self.arr[self.next_index]
+
+    def size(self):
+        return self.num_elements
+
+    def is_empty(self):
+        return self.num_elements == 0
+
+    def _handle_stack_capacity_full(self):
+        old_arr = self.arr
+
+        self.arr = [0 for _ in range( 2* len(old_arr))]
+        for index, element in enumerate(old_arr):
+            self.arr[index] = element
+obj1=Stack()
+obj2=Stack()
+obj2.push(4)
+obj1.push(2)
+obj1.push(3)
+print(obj1.arr)
+print(obj2.arr)
+print(obj1.size())
+print(obj1.next_index)
+print(obj1.pop())
+print(obj1.size())
+print(obj1.next_index)
