0ef9dcc48284574d271f27951baa850837364a18
Sonu Khan <64645655+sartaj-code@users.noreply.github.com>
2020-05-29 18:55:01 +0530

    Update stack_with_list.py

--- Stack/stack_with_list.py
+++ Stack/stack_with_list.py
@@ -1,1 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.item = []
+        self.num_item = 0
+        
+         #Initialize the Stack
+            
+    
+    def size(self):
+        if self.num_item == 0:
+            return self.num_item
+        else:
+            print(self.num_item)
+         #Check the size of the Stack
+    
+    def push(self, item1):
+        if len(self.item)==0:
+            self.item = item1
+        else:
+            self.item.append(item1)
+         #Push item onto Stack
 
+    def pop(self):
+        if len(self.item)==0:
+            return None
+        else:
+            self.item.pop()
+         #Pop item off of the Stack
+         
+MyStack = Stack()
+
+MyStack.push("Web Page 1")
+MyStack.push("Web Page 2")
+MyStack.push("Web Page 3")
+
+print (MyStack.item)
