31153800450e7957cd9450ba80964d46e0f28b47
Sonu Khan <64645655+sartaj-code@users.noreply.github.com>
2020-05-29 23:42:07 +0530

    Create queue_with_list.py

---
+++ Queue/queue_with_list.py
@@ -0,0 +1,30 @@
+class Queue:
+    def __init__(self):
+         self.storage = []
+    
+    def size(self):
+         return len(self.storage)
+    
+    def enqueue(self, item):
+         self.storage.append(item)
+
+    def dequeue(self):
+         return self.storage.pop(0)
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+print(q.storage)
+
+print ("Pass" if (q.size() == 3) else "Fail")
+
+print ("Pass" if (q.dequeue() == 1) else "Fail")
+
+q.enqueue(4)
+print ("Pass" if (q.dequeue() == 2) else "Fail")
+print ("Pass" if (q.dequeue() == 3) else "Fail")
+print(q.storage)
+print ("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print ("Pass" if (q.size() == 1) else "Fail")
