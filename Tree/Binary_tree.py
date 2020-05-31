d91191763b7c33b21695f263883adcc4956df85f
Sonu Khan <64645655+sartaj-code@users.noreply.github.com>
2020-06-01 03:41:07 +0530

    Create Binary_tree.py

---
+++ Tree/Binary_tree.py
@@ -0,0 +1,41 @@
+## Solution
+
+class Node(object):
+        
+    def __init__(self,value = None):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+    def set_value(self,value):
+        self.value = value
+        
+    def get_value(self):
+        return self.value
+        
+    def set_left_child(self,node):
+        self.left = node
+        
+    def set_right_child(self, node):
+        self.right = node
+        
+    def get_left_child(self):
+        return self.left
+    
+    def get_right_child(self):
+        return self.right
+
+
+    def has_left_child(self):
+        return self.left != None
+        
+    
+    def has_right_child(self):
+        return self.right != None
+        
+class Tree():
+    def __init__(self,value=None):
+        self.root = Node(value)
+        
+    def get_root(self):
+        return self.root        
